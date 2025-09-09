e09f9aa3a04fc4a98fd633a2cd35b17ee969a43b
kumar-kiran-24 <kirankumar26650@gmail.com>
2025-09-09 11:20:35 +0530

    add the logger and  exception files

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,21 @@
-import sys 
+import sys
 
-def error_mesage_deatil(error,erroe_detail:sys):
-    _,_,exc_tb=erroe_detail.exe_info()
-    file_name=exc_tb.tb_frame.f_code.co_filname
-    error_meaage="Error cocured in python script name[{0}] line number [{1}] erroe messaeg[{2}].format() "
-    file_name,exc_tb.tb_lineo,str(error)
-     
-class CustomExeception(Exception):
-    def __init__(self, error_message,error_detail:sys):
+def error_message_detail(error, error_detail: sys):
+    # Get exception details
+    _, _, exc_tb = sys.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    # Format the error message properly
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_number, str(error)
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_meaagse=error_mesage_deatil(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
